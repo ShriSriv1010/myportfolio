cd65227f7dc7a0ab1fa52cc328366c8694a7d1f1
ankitjha-webdev <190101120091@cutm.ac.in>
2022-07-01 05:18:40 +0530

    resume updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
-// import Testimonials from './components/testimonials/Testimonials';
 import Topbar from './components/topbar/Topbar';
 import Experience from './components/experience/Experience';
 import Volunteer from './components/experience/Volunteer';
@@ -27,7 +26,6 @@ const App = () => {
       <Volunteer/>
       <Projects />
       <Portfolio />
-      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
